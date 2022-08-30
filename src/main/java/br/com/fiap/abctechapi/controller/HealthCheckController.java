3bcaa39e97ff1aae9e7dd2872c44af37476b5cbd
samuelsp <samuel.cunha@prestadores.uhgbrasil.com.br>
2022-08-30 09:08:59 -0300

    first commit

---
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package br.com.fiap.abctechapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+    public ResponseEntity<Object> status() {
+        return ResponseEntity.ok().build();
+    }
+}
